2ebc9f1d467617b754abac110e89f43c9f021693
dzjhy <3178675106@qq.com>
2025-11-29 10:52:41 +0800

    创建Person/Employee/Visitor/RideInterface，完成Person

---
+++ src/Person.java
@@ -0,0 +1,47 @@
+public abstract class Person {
+
+    private String name;
+    private int age;
+    private String idCard;
+
+
+    public Person() {}
+
+
+    public Person(String name, int age, String idCard) {
+        this.name = name;
+        this.age = age;
+        this.idCard = idCard;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+
+    @Override
+    public String toString() {
+        return "姓名：" + name + "，年龄：" + age + "，身份证号：" + idCard;
+    }
+}
